412cf2fb373789e546342f704d3b10e56fd5376b
Sokaina Baouch <73339375+sb575@users.noreply.github.com>
2022-04-15 11:44:42 +0200

    Add recipe card detail done (#56)
    * Add search bar and search by categories done
    
    * Error in git actons tests fixed
    
    * Add recipe-card-detail done
    
    * Resolving conflicts

---
+++ frontend/src/app/recipe-card-detail/recipe-card-detail.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RecipeCardDetailComponent } from './recipe-card-detail.component';
+
+describe('RecipeCardDetailComponent', () => {
+  let component: RecipeCardDetailComponent;
+  let fixture: ComponentFixture<RecipeCardDetailComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ RecipeCardDetailComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RecipeCardDetailComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
