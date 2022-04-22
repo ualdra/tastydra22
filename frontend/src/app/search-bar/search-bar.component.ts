181d05f205c4be7233ed40a208bc1b44c8f856e7
Ángel Gómez Espinosa <73339908+age296@users.noreply.github.com>
2022-04-22 19:02:15 +0200

    search-bar
    searbar working and disable lazy load to avoid waste api calls

--- frontend/src/app/search-bar/search-bar.component.ts
+++ frontend/src/app/search-bar/search-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-search-bar',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchBarComponent implements OnInit {
 
+  @Output() searchRecipe = new EventEmitter<string>();
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  addNewItem(value: string) {
+    this.searchRecipe.emit(value);
+  }
 }
