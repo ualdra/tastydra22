79e0d4dee278af3c139b24f6d29fee3ed8149959
ale-gen <o.generowicz@gmail.com>
2022-04-22 12:14:13 +0200

    Create entities

---
+++ backend/src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,88 @@
+package com.example.demo.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotBlank(message = "Name is mandatory")
+    private String name;
+
+    @Email
+    @NotBlank(message = "Email is mandatory")
+    private String email;
+
+    @NotBlank(message = "Password is mandatory")
+    private String password;
+    
+    @NotBlank(message = "Token is mandatory")
+    private String token;
+
+    public User() {}
+
+    public User(String name, String email, String password, String token) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.token = token;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    @Override
+    public String toString() {
+        return "User: " + id + "\nName: " + name + "\nEmail: " + email + 
+            "\nPassword: " + password + "\nToken: " + token;
+    }
+
+}
