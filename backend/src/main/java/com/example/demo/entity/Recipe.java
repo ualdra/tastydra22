7d8ef62bdae4d62eb95cc113128087fdc1fc1da5
A.Generowicz <62150130+ale-gen@users.noreply.github.com>
2022-04-22 17:48:26 +0200

    Create entities for the backend #35  (#62)
    * Create entities
    
    * Add getter for meal type
    
    * Add user's recipes
    
    * Add max length for attributes

---
+++ backend/src/main/java/com/example/demo/entity/Recipe.java
@@ -0,0 +1,64 @@
+package com.example.demo.entity;
+
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.Length;
+
+@Entity
+@Table(name = "recipes")
+public class Recipe {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotNull(message = "Date is mandatory")
+    private Date date;
+
+    @NotBlank(message = "Meal type is mandatory")
+    @Length(max = 15)
+    private String mealType;
+
+    public Recipe() {}
+
+    public Recipe(Date date, String mealType) {
+        this.date = date;
+        this.mealType = mealType;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getMealType() {
+        return mealType;
+    }
+
+    public void setMealType(String mealType) {
+        this.mealType = mealType;
+    }
+
+    @Override
+    public String toString() {
+        return "Recipe: " + id + "\nDate: " + date + "\nMeal type: " + mealType;
+    }
+
+}
