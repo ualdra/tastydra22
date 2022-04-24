effed21a9b6bb710c2adf40087e45e0eda5c8a84
Maria Galvez Gonzalez <margalgon@hotmail.es>
2022-04-24 21:15:36 +0200

    Sign up page and password fields

---
+++ frontend/src/app/sign-up/sign-up.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.scss']
+})
+export class SignUpComponent implements OnInit {
+  hide_password = true;
+  hide_repeat = true;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
