f70996cc9b783143f37001bbf71e1a11ba66f0c9
Cristian David Casado <cristiancasado27@gmail.com>
2022-05-21 18:35:45 +0200

    menu's recipes listed

--- frontend/src/app/menu-card/menu-card.component.ts
+++ frontend/src/app/menu-card/menu-card.component.ts
@@ -7,6 +7,9 @@ import {
 } from '@angular/material/dialog';
 import { RecipeDialogComponent } from '../recipe-dialog/recipe-dialog.component';
 import { MenuData } from '../menu-data';
+import { ApiTastyServiceService } from '../api-tasty-service.service';
+import { Recipe as TastyRecipe } from '../api-tasty';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu-card',
@@ -15,19 +18,52 @@ import { MenuData } from '../menu-data';
 })
 export class MenuCardComponent implements OnInit {
   editItems = false;
-  menuTypes = ["Breakfast", "Lunch", "Dinner"];
   @Input() index?: number;
   menuInfo?: MenuData;
+
   @Input() listOfRecipes: Recipe[] = [];
+  dayRecipes?: Recipe[];
+
+  breakfastRecipes?: TastyRecipe[] = [];
+  lunchRecipes?: TastyRecipe[] = [];
+  dinnerRecipes?: TastyRecipe[] = [];
 
   constructor(
-    public dialog: MatDialog
-    ) { }
+    public dialog: MatDialog,
+    private tastyService: ApiTastyServiceService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
     var date = new Date();
     date.setDate(date.getDate() + this.index!)
     this.menuInfo = { date: date, type: '', isEdit: true };
+    // WARNING, DATES CAN HAVE DIFFERENT FORMATS
+    this.dayRecipes = this.listOfRecipes.filter( recipe => recipe.date === this.menuInfo?.date.toDateString());
+    this.clasifier();
+  }
+
+  clasifier(): void {
+    for (let recipe of this.dayRecipes!) {
+      if (recipe.mealType === "breakfast") {
+        this.tastyService.getRecipe(recipe.mealId).subscribe( tastyRecipe => {
+          this.breakfastRecipes?.push(tastyRecipe);
+        })
+      } else if (recipe.mealType === "lunch") {
+        this.tastyService.getRecipe(recipe.mealId).subscribe( tastyRecipe => {
+          this.lunchRecipes?.push(tastyRecipe);
+        })
+      } else {
+        this.tastyService.getRecipe(recipe.mealId).subscribe( tastyRecipe => {
+          this.dinnerRecipes?.push(tastyRecipe);
+        })
+      }
+    }
+  }
+
+  openRecipe(recipe: TastyRecipe) {
+    if (this.editItems) return;
+    this.router.navigate(['/recipe', {id: recipe.id}]);
   }
 
   openDialog(): void {
