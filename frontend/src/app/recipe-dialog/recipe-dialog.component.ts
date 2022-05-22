3ea17bd971941cb5b42971622f469a76ed969423
Ángel Gómez Espinosa <73339908+age296@users.noreply.github.com>
2022-05-22 13:08:08 +0200

    Adding recipes to user (#148)
    * Adding recipe to user
    
    A loged user can add a recipe.
    
    * Update recipe-dialog.component.ts
    
    * fix conflict
    
    fix a conflict

--- frontend/src/app/recipe-dialog/recipe-dialog.component.ts
+++ frontend/src/app/recipe-dialog/recipe-dialog.component.ts
@@ -39,7 +39,7 @@ export class RecipeDialogComponent implements OnInit {
           (this.user = us)
         });
     this.isEdit = data.isEdit;
-    this.idRecipe =  data.idRecipe;
+    this.idRecipe =  data.recipe.id;
   }
 
   ngOnInit(): void {
@@ -51,6 +51,12 @@ export class RecipeDialogComponent implements OnInit {
 
   onSave(): void {
     
+    let menuInfo: MenuData = {
+      date: this.dateSelected,
+      type: this.typeSelected,
+      isEdit: this.isEdit,
+    };
+    
     let recipe: Recipe = {
       mealId: this.idRecipe,
       date: this.dateSelected,
@@ -59,7 +65,7 @@ export class RecipeDialogComponent implements OnInit {
     };
     this.recipeService.addRecipeToUser(this.user.id, recipe).subscribe(
       () => {
-        this.dialogRef.close(recipe);
+        this.dialogRef.close(menuInfo);
       }
     );
     
